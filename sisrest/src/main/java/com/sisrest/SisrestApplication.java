0e8ef8a3b4ff2647c9f63652a6cdef02d1c40295
Patrícia dos Santos Pereira <patricia.pereira@academico.ifpb.edu.br>
2022-12-11 18:00:05 -0300

    Initial commit

---
+++ sisrest/src/main/java/com/sisrest/SisrestApplication.java
@@ -0,0 +1,13 @@
+package com.sisrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisrestApplication.class, args);
+	}
+
+}
---
+++ sisrest/src/test/java/com/sisrest/SisrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
