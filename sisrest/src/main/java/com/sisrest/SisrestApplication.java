408cf64b39c4b36b348aaf60a7d10dc038ec1f06
Rosenato Barreto <rosenatoblima@gmail.com>
2022-12-21 01:48:43 -0300

    Criação de classe de configuração de CORS

--- sisrest/src/main/java/com/sisrest/SisrestApplication.java
+++ sisrest/src/main/java/com/sisrest/SisrestApplication.java
@@ -2,13 +2,26 @@ package com.sisrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SisrestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SisrestApplication.class, args); 
+		SpringApplication.run(SisrestApplication.class, args);
 	}
+
+//	@Bean
+//	 public WebMvcConfigurer configure() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry reg) {
+//				reg.addMapping("/**").allowedOrigins("*");
+//			}
+//	  };
+//	}
 
 }
---
+++ sisrest/src/main/java/com/sisrest/configuration/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.sisrest.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+    }
+}
