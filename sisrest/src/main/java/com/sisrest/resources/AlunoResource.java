f3d10801160ad1e2be05c2539ded63e1d0eeb2a0
Gabriel <gabriel.pb.congo@gmail.com>
2022-12-28 17:10:39 -0300

    Metodo recuperar DadosCSV

--- sisrest/src/main/java/com/sisrest/resources/AlunoResource.java
+++ sisrest/src/main/java/com/sisrest/resources/AlunoResource.java
@@ -1,20 +1,16 @@
 package com.sisrest.resources;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 import com.sisrest.storage.Disco;
 
@@ -27,18 +23,14 @@ public class AlunoResource {
 
 	@PostMapping(value = "/uploadCSV")
 	public void uploadCSV(@RequestParam("file") MultipartFile arquivoCSV) throws IOException, CsvException {
-
 		disco.salvarCSV(arquivoCSV);
-
-//		File file = null;
-//		arquivoCSV.transferTo(file);
-//		try (CSVReader csvReader = new CSVReader(new FileReader(file))) {
-//			List<String[]> linhas = csvReader.readAll();
-//			return new ResponseEntity(linhas, HttpStatus.OK);
-//		} catch (Exception e) {
-//			// TODO: handle exception
-//		}
-//		return new ResponseEntity("Não deu certo", HttpStatus.NOT_FOUND);
-//		
+		
 	}
+	
+	@GetMapping(value = "/recuperarCSV")
+	public List<String[]> recuperarCSV() throws CsvException{
+		return disco.recuperarCSV();
+	}
+		
+
 }
--- sisrest/src/main/java/com/sisrest/storage/Disco.java
+++ sisrest/src/main/java/com/sisrest/storage/Disco.java
@@ -1,22 +1,37 @@
 package com.sisrest.storage;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+
 @Component
 public class Disco {
-
+	
+	private static final Disco INSTANCIA_DISCO = new Disco();
+	
+	private Disco() {}
+	
+	public Disco getInstancia() {
+		return INSTANCIA_DISCO;
+	}
 	@Value("D:\\GitHub\\Sisrest\\sisrest\\src\\resources\\files")
 	private String raiz;
 
 	@Value("CSV")
 	private String diretorioCSV;
+
+	private File file;
 
 	public void salvarCSV(MultipartFile arquivoCSV) {
 		this.salvar(this.diretorioCSV, arquivoCSV);
@@ -25,12 +40,24 @@ public class Disco {
 	private void salvar(String diretorio, MultipartFile arquivo) {
 		Path diretorioPath = Paths.get(this.raiz, diretorio);
 		Path arquivoPath = diretorioPath.resolve(arquivo.getOriginalFilename());
-
+		file = arquivoPath.toFile();
 		try {
 			Files.createDirectories(diretorioPath);
 			arquivo.transferTo(arquivoPath.toFile());
+
 		} catch (IOException e) {
 			throw new RuntimeException("Erro ao Salvar!!");
 		}
+	}
+
+	public List<String[]> recuperarCSV() throws CsvException {
+
+		try (CSVReader csvReader = new CSVReader(new FileReader(this.file))) {
+			List<String[]> linhas = csvReader.readAll();
+			return linhas;
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao Recuperar!");
+		}
+		
 	}
 }
