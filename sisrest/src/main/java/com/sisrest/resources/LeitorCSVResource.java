26fa88e168b43ee942cfa1befda85620c9128bfb
Gabriel <gabriel.pb.congo@gmail.com>
2022-12-28 00:38:57 -0300

    Criação da classe Disco para salvar o CSV

--- sisrest/src/main/java/com/sisrest/resources/LeitorCSVResource.java
+++ sisrest/src/main/java/com/sisrest/resources/LeitorCSVResource.java
@@ -1,37 +1,37 @@
 package com.sisrest.resources;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import com.sisrest.storage.Disco;
 
 @RestController
 @RequestMapping("/api/lerArquivoCSV")
 public class LeitorCSVResource {
 
-	@PostMapping
-	public ResponseEntity<String[]> uploadCSV(@RequestParam("file") MultipartFile arquivoCSV)
-			throws IOException, CsvException {
-		File file = null;
-		arquivoCSV.transferTo(file);
-		try (CSVReader csvReader = new CSVReader(new FileReader(file))) {
-			List<String[]> linhas = csvReader.readAll();
-			return new ResponseEntity(linhas, HttpStatus.OK);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-		return new ResponseEntity("Não deu certo", HttpStatus.NOT_FOUND);
+	@Autowired
+	private Disco disco;
 
+	@PostMapping
+	public void uploadCSV(@RequestParam("file") MultipartFile arquivoCSV) throws IOException, CsvException {
+
+		disco.salvarCSV(arquivoCSV);
+
+//		File file = new File("D:\\GitHub\\Sisrest\\sisrest");
+//		arquivoCSV.transferTo(file);
+//		try (CSVReader csvReader = new CSVReader(new FileReader(file))) {
+//			List<String[]> linhas = csvReader.readAll();
+//			return new ResponseEntity(linhas, HttpStatus.OK);
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+//		return new ResponseEntity("Não deu certo", HttpStatus.NOT_FOUND);
+//
 	}
 }
---
+++ sisrest/src/main/java/com/sisrest/storage/Disco.java
@@ -0,0 +1,36 @@
+package com.sisrest.storage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class Disco {
+
+	@Value("..D")
+	private String raiz;
+
+	@Value("..Sisrest/src/main/java/com/sisrest/files")
+	private String diretorioCSV;
+
+	public void salvarCSV(MultipartFile arquivoCSV) {
+		this.salvar(this.diretorioCSV, arquivoCSV);
+	}
+
+	private void salvar(String diretorio, MultipartFile arquivo) {
+		Path diretorioPath = Paths.get(this.raiz, diretorio);
+		Path arquivoPath = diretorioPath.resolve(arquivo.getOriginalFilename());
+
+		try {
+			Files.createDirectories(diretorioPath);
+			arquivo.transferTo(arquivoPath.toFile());
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao Salvar!!");
+		}
+	}
+}
