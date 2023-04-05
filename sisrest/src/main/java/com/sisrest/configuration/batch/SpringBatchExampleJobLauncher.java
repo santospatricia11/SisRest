4dd5a1f177d2efac8b6495bc4cd57d2d4c68770c
Gabriel Oliveira <gabriel.pb.congo@gmail.com>
2023-04-05 14:50:54 -0300

    atualizações

--- sisrest/src/main/java/com/sisrest/configuration/batch/SpringBatchExampleJobLauncher.java
+++ sisrest/src/main/java/com/sisrest/configuration/batch/SpringBatchExampleJobLauncher.java
@@ -15,7 +15,6 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
--- sisrest/src/main/java/com/sisrest/repositories/BeneficiarioRepository.java
+++ sisrest/src/main/java/com/sisrest/repositories/BeneficiarioRepository.java
@@ -3,7 +3,6 @@ package com.sisrest.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sisrest.model.entities.Aluno;
 import com.sisrest.model.entities.Beneficiario;
 
 @Repository
